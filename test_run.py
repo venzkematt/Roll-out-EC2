4b6c70ff232f5ae3bb02679e69f900efb509b8fb
venzkematt <venzkematt@aol.com>
2023-08-21 16:07:47 +0200

    fail

--- test_run.py
+++ test_run.py
@@ -3,7 +3,7 @@ from run import checkInt
 
 class TestCheckInt(unittest.TestCase):
     def test_checkInt(self):
-        self.assertEqual (checkInt(type(90)), int)
+        self.assertEqual (checkInt(type(9.0)), int)
 
 if __name__ == "__main__":
     unittest.main()
