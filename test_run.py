b49b4e36dc8a13953c5efc1fd393e86808866e1e
venzkematt <venzkematt@aol.com>
2023-08-21 16:40:14 +0200

    addEC2

--- test_run.py
+++ test_run.py
@@ -3,7 +3,7 @@ from run import checkInt
 
 class TestCheckInt(unittest.TestCase):
     def test_checkInt(self):
-        self.assertEqual (checkInt(type(99)), int)
+        self.assertEqual (checkInt(type(33)), int)
 
 if __name__ == "__main__":
     unittest.main()
